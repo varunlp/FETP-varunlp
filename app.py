59093b7d720e4ee199c94ed817850633b27d491d
Varun L P <vkpleela@gmail.com>
2023-11-09 17:07:33 +0530

    committed on 1707 9 11 23

--- app.py
+++ app.py
@@ -1,28 +1,36 @@
-from flask import Flask, render_template, redirect, url_for, session
+from flask import Flask, render_template, redirect, url_for, session, request
 import datetime
-import pytz  
+import pytz
 
-app = Flask(__name__) 
-app.secret_key = '2001'
+app = Flask(__name__)
+app.secret_key = 'varun'
 
 @app.route('/')
-def home():
-    user_info = session.get('user_info')
-    current_time = get_indian_time()
-    return render_template('index.html', user_info=user_info, current_time=current_time)
+def login_page():
+    return render_template('login.html')
 
-def get_indian_time():
-    indian_timezone = pytz.timezone('Asia/Kolkata')
-    indian_time = datetime.datetime.now(indian_timezone).strftime('%Y-%m-%d %H:%M:%S')
-    return indian_time
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    if 'user_info' in session:
+        user_info = session['user_info']
+        current_time = get_indian_time()
+
+        if request.method == 'POST':
+            num_lines = int(request.form.get('num_lines', 0))
+            design_output = generate_design(num_lines)
+            return render_template('home.html', user_info=user_info, current_time=current_time, design_output=design_output)
+
+        return render_template('home.html', user_info=user_info, current_time=current_time)
+
+    return redirect(url_for('login_page'))
 
 @app.route('/login')
 def login():
-    
+    # Simulate Gmail authentication
     user_info = {
         'name': 'Varun L P',
         'email': 'vkpleela@gmail.com',
-        'profile_picture': 'varun1.jpg'  
+        'profile_picture': 'varun1.jpg'  # Optional
     }
 
     session['user_info'] = user_info
@@ -31,7 +39,42 @@ def login():
 @app.route('/logout')
 def logout():
     session.pop('user_info', None)
-    return redirect(url_for('home'))
+    return redirect(url_for('login_page'))
+
+def get_indian_time():
+    indian_timezone = pytz.timezone('Asia/Kolkata')
+    indian_time = datetime.datetime.now(indian_timezone).strftime('%Y-%m-%d %H:%M:%S')
+    return indian_time
+
+
+import random
+
+
+def generate_design(num_lines):
+    design = ""
+    letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    for i in range(0, num_lines):
+        spaces = " " * (num_lines - i - 1)
+        random_letters = ''.join(random.choice(letters) for _ in range(2 * i + 1))
+        design += spaces + random_letters + "\n"
+
+    for i in range(num_lines - 2, -1, -1):
+        spaces = " " * (num_lines - i - 1)
+        random_letters = ''.join(random.choice(letters) for _ in range(2 * i + 1))
+        design += spaces + random_letters + "\n"
+
+    return design
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
