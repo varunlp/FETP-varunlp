0668d029aa662f67c1324905f8ef0af7cb4e24a7
Varun L P <vkpleela@gmail.com>
2023-11-09 11:50:29 +0530

    this are all my first work

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, redirect, url_for, session
+import datetime
+import pytz  # This is a library to work with time zones
+
+app = Flask(__name__)
+app.secret_key = '2001'
+
+@app.route('/')
+def home():
+    user_info = session.get('user_info')
+    current_time = get_indian_time()
+    return render_template('index.html', user_info=user_info, current_time=current_time)
+
+def get_indian_time():
+    indian_timezone = pytz.timezone('Asia/Kolkata')
+    indian_time = datetime.datetime.now(indian_timezone).strftime('%Y-%m-%d %H:%M:%S')
+    return indian_time
+
+@app.route('/login')
+def login():
+    # Simulate Gmail authentication
+    user_info = {
+        'name': 'Varun L P',
+        'email': 'vkpleela@gmail.com',
+        'profile_picture': 'varun1.jpg'  
+    }
+
+    session['user_info'] = user_info
+    return redirect(url_for('home'))
+
+@app.route('/logout')
+def logout():
+    session.pop('user_info', None)
+    return redirect(url_for('home'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
