4c3b98f9199d55225e5ea572a425735de7204a7d
Varun L P <vkpleela@gmail.com>
2023-11-10 12:06:54 +0530

    tried with oauth day1 code

--- README.md
+++
@@ -1,5 +0,0 @@
-# FETP-varunlp
-simple Flask application 
-Learning github so tried different actions like push pull commit etc
-Thanking formula Q For this learning initiative.
-............
--- app.py
+++ app.py
@@ -1,80 +1,70 @@
-from flask import Flask, render_template, redirect, url_for, session, request
-import datetime
-import pytz
+from flask import Flask, redirect, url_for, session, render_template
+from flask_oauthlib.client import OAuth
+from datetime import datetime
+import os
+from collections.abc import MutableMapping
 
 app = Flask(__name__)
-app.secret_key = 'varun'
+app.secret_key = 'your_secret_key'
+app.config['GOOGLE_ID'] = os.environ.get('GOOGLE_CLIENT_ID')  # Set your environment variable for Google Client ID
+app.config['GOOGLE_SECRET'] = os.environ.get('GOOGLE_CLIENT_SECRET')  # Set your environment variable for Google Client Secret
+oauth = OAuth(app)
+
+google = oauth.remote_app(
+    'google',
+    consumer_key=app.config.get('GOOGLE_ID'),
+    consumer_secret=app.config.get('GOOGLE_SECRET'),
+    request_token_params={'scope': 'email'},
+    base_url='https://www.googleapis.com/oauth2/v1/',
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    request_token_url=None,
+    access_token_method='POST',
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    access_token_params=None,
+)
 
 @app.route('/')
-def login_page():
-    return render_template('login.html')
-
-@app.route('/home', methods=['GET', 'POST'])
 def home():
-    if 'user_info' in session:
-        user_info = session['user_info']
-        current_time = get_indian_time()
+    if 'google_token' in session:
+        user_info = google.get('userinfo')
 
-        if request.method == 'POST':
-            num_lines = int(request.form.get('num_lines', 0))
-            design_output = generate_design(num_lines)
-            return render_template('home.html', user_info=user_info, current_time=current_time, design_output=design_output)
+        # Get user details
+        user_name = user_info.data.get('name', 'User')
+        user_email = user_info.data.get('email', 'user@example.com')
 
-        return render_template('home.html', user_info=user_info, current_time=current_time)
+        # Get current time in India
+        india_timezone = 'Asia/Kolkata'
+        current_time = datetime.now().astimezone(timezone(india_timezone)).strftime('%Y-%m-%d %H:%M:%S')
 
-    return redirect(url_for('login_page'))
+        return render_template('home.html', user_name=user_name, user_email=user_email, current_time=current_time)
+    else:
+        return redirect(url_for('login'))
 
 @app.route('/login')
 def login():
-   
-    user_info = {
-        'name': 'Varun L P',
-        'email': 'vkpleela@gmail.com',
-        'profile_picture': 'varun1.jpg'  # Optional
-    }
-
-    session['user_info'] = user_info
-    return redirect(url_for('home'))
+    return google.authorize(callback=url_for('authorized', _external=True))
 
 @app.route('/logout')
 def logout():
-    session.pop('user_info', None)
-    return redirect(url_for('login_page'))
+    session.pop('google_token', None)
+    return redirect(url_for('login'))
 
-def get_indian_time():
-    indian_timezone = pytz.timezone('Asia/Kolkata')
-    indian_time = datetime.datetime.now(indian_timezone).strftime('%Y-%m-%d %H:%M:%S')
-    return indian_time
+@app.route('/login/authorized')
+def authorized():
+    response = google.authorized_response()
+    if response is None or response.get('access_token') is None:
+        return 'Access denied: reason={} error={}'.format(
+            request.args['error_reason'],
+            request.args['error_description']
+        )
 
+    session['google_token'] = (response['access_token'], '')
+    user_info = google.get('userinfo')
+    return redirect(url_for('home'))
 
-import random
-
-
-def generate_design(num_lines):
-    design = ""
-    letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-    for i in range(0, num_lines):
-        spaces = " " * (num_lines - i - 1)
-        random_letters = ''.join(random.choice(letters) for _ in range(2 * i + 1))
-        design += spaces + random_letters + "\n"
-
-    for i in range(num_lines - 2, -1, -1):
-        spaces = " " * (num_lines - i - 1)
-        random_letters = ''.join(random.choice(letters) for _ in range(2 * i + 1))
-        design += spaces + random_letters + "\n"
-
-    return design
-
-
-
-
-
-
-
-
-
-
+@oauth.tokengetter
+def get_google_oauth_token():
+    return session.get('google_token')
 
 if __name__ == '__main__':
     app.run(debug=True)
